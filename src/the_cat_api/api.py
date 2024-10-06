784f8b055a3bcf2b688efe90c5af3d0206dcd403
ShadowGry <54568043+ShadowGry@users.noreply.github.com>
2024-10-06 18:23:00 +0800

    Add search images

--- src/the_cat_api/api.py
+++ src/the_cat_api/api.py
@@ -5,3 +5,9 @@ class TheCatAPI(Client):
 
     def __init__(self, key: str):
         super().__init__(key)
+
+    async def search_images(self):
+        """
+        Search all approved images.
+        """
+        return await self.get('/v1/images/search')
--- src/the_cat_api/client.py
+++ src/the_cat_api/client.py
@@ -11,10 +11,13 @@ class Client:
     async def close(self):
         await self.session.close()
 
-    async def request(self, method, endpoint):
+    async def request(self, method, endpoint, parameters=None):
         headers = {
             'x-api-key': self.key
         }
-        async with self.session.request(method, self.host + endpoint, headers=headers) as response:
+        async with self.session.request(method, self.host + endpoint, params=parameters, headers=headers) as response:
             content = await response.json()
         return content
+
+    async def get(self, endpoint, **kwargs):
+        return await self.request('GET', endpoint, **kwargs)
