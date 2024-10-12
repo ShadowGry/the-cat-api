3ea25be73641b7f0333e93ab0612a28db7517544
ShadowGry <54568043+ShadowGry@users.noreply.github.com>
2024-10-12 20:33:12 +0800

    Add parameters to search images

--- src/the_cat_api/api.py
+++ src/the_cat_api/api.py
@@ -6,8 +6,31 @@ class TheCatAPI(Client):
     def __init__(self, key: str, version: int = 1):
         super().__init__(key, version)
 
-    async def search_images(self):
+    async def search_images(
+        self,
+        size: str = 'med',
+        mime_types: str = 'jpg',
+        format: str = 'json',
+        has_breeds: bool = True,
+        order: str = 'RANDOM',
+        page: int = 0,
+        limit: int = 1,
+        include_breeds: int = 1,
+        include_categories: int = 1
+    ):
         """
-        Search all approved images.
+        Search all approved images. 
         """
-        return await self.get('/images/search')
+        parameters = {
+            'size': size,
+            'mime_types': mime_types,
+            'format': format,
+            'has_breeds': str(has_breeds).lower(),
+            'order': order,
+            'page': page,
+            'limit': limit,
+            'include_breeds': include_breeds,
+            'include_categories': include_categories
+        }
+        # TODO: Handle format=src response
+        return await self.get('/images/search', parameters=parameters)
